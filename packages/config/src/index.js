165aaa67afd4dd1a0196adcb302367252b42042c
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-01-31 16:08:43 -0500

    uses oracle smart contract package and relative imports in licensing pkg

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,15 +1,40 @@
 import Web3 from 'web3';
 
+import { USDETHOracle, TestOracle } from '../../contracts/oracle';
+import { getContractAddress } from '../../utils';
+
 import ujoStorage from './ujoStorage';
 
 export default function ujoInit(web3Provider, dataStorageProvider, opts = {}) {
   // TODO: add network validations (rinkeby or mainnet)
   const web3 = new Web3(web3Provider);
   const storageProvider = ujoStorage(dataStorageProvider);
+  const Oracle = opts.test ? TestOracle : USDETHOracle;
   return {
     // returns the web3 instance
     getWeb3: () => web3,
     getStorageProvider: () => storageProvider,
+    getOracleAddress: () =>
+      new Promise((resolve, reject) => {
+        web3.eth.net.getId((err, networkId) => {
+          if (err) reject(err);
+          else resolve(getContractAddress(Oracle, networkId));
+        });
+      }),
+    getExchangeRate: () =>
+      new Promise((resolve, reject) => {
+        web3.eth.net.getId(async (err, networkId) => {
+          if (err) reject(err);
+          try {
+            const oracleAddress = getContractAddress(Oracle, networkId);
+            const oracleInstance = new web3.eth.Contract(Oracle.abi, oracleAddress);
+            const exchangeRate = await oracleInstance.methods.getUintPrice().call();
+            resolve(exchangeRate.toString(10));
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }),
     // return the accounts given by the provider
     getAccounts: () =>
       new Promise((resolve, reject) => {
--- packages/contracts/licensing/index.js
+++ packages/contracts/licensing/index.js
@@ -1,2 +1,1 @@
 exports.ETHUSDHandler = require('./build/contracts/ETHUSDHandler.json');
-exports.TestOracle = require('./build/contracts/TestOracle.json');
